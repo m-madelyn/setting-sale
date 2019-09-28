954c7920d09435d834352bd092502a4286ae159b
m-madelyn <m.madelyn@live.com>
2019-09-28 16:07:01 -0400

    implements selectSaleItems function

--- checkout.js
+++ checkout.js
@@ -1,5 +1,11 @@
+function ReturnSaleItems(saledata) {
+  const discountamt = saledata.discount  !== 0 && saledata.discount !== undefined
+
+  return discountamt
+}
+
 function selectSaleItems(items) {
-  return items
+return items.filter(ReturnSaleItems)
 }
 
 module.exports = selectSaleItems
